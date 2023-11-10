850612b79ac5d2721fa2a5b217ab39569637808c
samdeveloper23 <sam.developer.23@gmail.com>
2023-11-10 08:10:56 +0100

    siubida

---
+++ src/screens/Frame/Frame.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./style.css";
+
+export const Frame = () => {
+  return (
+    <div className="frame">
+      <div className="div">
+        <img className="whatsapp-image" alt="Whatsapp image" src="/img/whatsapp-image-2023-11-09-at-19-56-1.png" />
+        <div className="AYUDA-te-estoy">&nbsp;&nbsp;AYUDA! te estoy buscando</div>
+        <p className="estoy-abandonado-en">
+          Estoy abandonado en una obra con mucho frío, me cuidan los vigilantes pero soy bueno, ¿por qué no tengo una
+          familia que me quiera? ¿me quieres?
+          <br />
+          Mvl: 654256127
+          <br />
+          Preguntar por Sam
+        </p>
+        <img className="img" alt="Whatsapp image" src="/img/whatsapp-image-2023-11-09-at-19-56-2.png" />
+        <p className="text-wrapper">Gracias, no tengo ni un año... Por cierto, me llamo Zeus</p>
+      </div>
+    </div>
+  );
+};
